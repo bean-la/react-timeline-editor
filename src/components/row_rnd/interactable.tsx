ae0d4b5d99eacb6d1af355e483376ca1e61a41af
Sebastian Bean <seb@bean.la>
2025-01-12 19:49:11 -0800

    fix for react 18

--- src/components/row_rnd/interactable.tsx
+++ src/components/row_rnd/interactable.tsx
@@ -2,15 +2,15 @@ import { DraggableOptions } from "@interactjs/actions/drag/plugin";
 import { ResizableOptions } from "@interactjs/actions/resize/plugin";
 import { DragEvent, Interactable } from "@interactjs/types";
 import interact from "interactjs";
-import { cloneElement, FC, ReactElement, useEffect, useRef } from "react";
+import { cloneElement, FC, ReactElement, useEffect, useRef, PropsWithChildren } from "react";
 
-export const InteractComp: FC<{
+export const InteractComp: FC<PropsWithChildren<{
   interactRef?: React.MutableRefObject<Interactable>;
   draggable: boolean;
   draggableOptions: DraggableOptions;
   resizable: boolean;
   resizableOptions: ResizableOptions;
-}> = ({ children, interactRef, draggable, resizable, draggableOptions, resizableOptions }) => {
+}>> = ({ children, interactRef, draggable, resizable, draggableOptions, resizableOptions }) => {
   const nodeRef = useRef<HTMLElement>();
   const interactable = useRef<Interactable>();
   const draggableOptionsRef = useRef<DraggableOptions>();
@@ -36,7 +36,7 @@ export const InteractComp: FC<{
         onmove: (e) => draggableOptionsRef.current.onmove && (draggableOptionsRef.current.onmove as (e: DragEvent) => any)(e),
         onend: (e) => draggableOptionsRef.current.onend && (draggableOptionsRef.current.onend as (e: DragEvent) => any)(e),
       });
-    if (resizable) interactable.current.resizable({ 
+    if (resizable) interactable.current.resizable({
       ...resizableOptionsRef.current,
       onstart: (e) => resizableOptionsRef.current.onstart && (resizableOptionsRef.current.onstart as (e: DragEvent) => any)(e),
       onmove: (e) => resizableOptionsRef.current.onmove && (resizableOptionsRef.current.onmove as (e: DragEvent) => any)(e),
